450add61d659d94eb448440512f845c25466cce4
nowwuh <83020117+nowwuh@users.noreply.github.com>
2021-04-22 10:38:59 -0400

    Update main.blocks, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -6,20 +6,20 @@ namespace myImages {
             case "image1":
             case "myImage":return img`
 . . . . . . . . . . . . . . . . 
-. . . . . . 3 3 3 3 3 3 3 3 . . 
-. . . . . 3 c 3 3 3 3 3 3 d 3 . 
-. . . . 3 c c 3 3 3 3 3 3 d c 3 
-. . d 3 d c c 3 d d d d d d c c 
-. d 3 3 d c b a a a a a a a 3 c 
-. 3 3 3 d b a a b b b a b b a 3 
-. 3 3 3 3 3 a b b b b a b b b a 
-. 3 3 3 3 a 3 3 3 3 3 a 3 3 3 a 
-. 3 d d 3 a f a a a f a a a a a 
-. d d 3 a a a f a a f a a a a a 
-. a a a a a a a f f f a a a a a 
-. a a a a f f f a a a a f f f f 
-. . . a f f f f f a a f f f f f 
-. . . . f f f f . . . . f f f . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f . . . . f . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . f f f 1 f f 1 f f f . . . 
+. . f f . f f f f f f . f f . . 
+. . f . . f f f f f f . . f . . 
+. . . . . f f f f f f . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . . f . . f . . . . . . 
+. . . . f f f . . f f f . . . . 
+. . . . f . f . . f . f . . . . 
+. . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
         }
